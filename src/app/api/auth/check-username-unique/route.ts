63dde45f0a2537345b1ff58b45dbd76de4b0d937
AbhishekTiwari14 <abhishek.tiwari2003@gmail.com>
2025-04-30 16:49:22 +0530

    solved write testimonials page issue with search params

--- src/app/api/auth/check-username-unique/route.ts
+++ src/app/api/auth/check-username-unique/route.ts
@@ -1,67 +1,69 @@
-import { z } from 'zod';
-import { usernameValidation } from '@/schemas/signUpSchema';
-import connectDb from '@/lib/connectDb';
-import UserModel from '@/app/models/User';
+import { z } from "zod"
+import { usernameValidation } from "@/schemas/signUpSchema"
+import connectDb from "@/lib/connectDb"
+import UserModel from "@/app/models/User"
+
+export const dynamic = "force-dynamic"
 
 const UsernameQuerySchema = z.object({
   username: usernameValidation,
-});
+})
 
 export async function GET(request: Request) {
-  await connectDb();
+  await connectDb()
 
   try {
-    const { searchParams } = new URL(request.url);
+    const { searchParams } = new URL(request.url)
 
     const queryParams = {
-      username: searchParams.get('username'),
-    };
+      username: searchParams.get("username"),
+    }
 
-    const result = UsernameQuerySchema.safeParse(queryParams);
+    const result = UsernameQuerySchema.safeParse(queryParams)
 
     if (!result.success) {
-      const usernameErrors = result.error.format().username?._errors || [];
+      const usernameErrors = result.error.format().username?._errors || []
       return Response.json(
         {
           success: false,
           message:
             usernameErrors?.length > 0
-              ? usernameErrors.join(', ')
-              : 'Invalid query parameters',
+              ? usernameErrors.join(", ")
+              : "Invalid query parameters",
         },
         { status: 400 }
-      );
+      )
     }
 
-    const { username } = result.data;
+    const { username } = result.data
 
-    const existingUser = await UserModel.findOne({username});
+    const existingUser = await UserModel.findOne({ username })
 
     if (existingUser) {
       return Response.json(
         {
           success: false,
-          message: 'Username is already taken',
+          message: "Username is already taken",
         },
         { status: 200 }
-      );
+      )
     }
 
     return Response.json(
       {
         success: true,
-        message: 'Username is unique',
+        message: "Username is unique",
       },
       { status: 200 }
-    );
+    )
   } catch (error) {
-    console.error('Error checking username:', error);
+    console.error("Error checking username:", error)
     return Response.json(
       {
         success: false,
-        message: 'Error checking username',
+        message: "Error checking username",
       },
       { status: 500 }
-    );
+    )
   }
 }
