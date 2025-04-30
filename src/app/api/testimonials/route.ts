aedbb158539686c5a07e1af06eac9b975cac9207
AbhishekTiwari14 <abhishek.tiwari2003@gmail.com>
2025-04-30 16:39:31 +0530

    solved type error

--- src/app/api/testimonials/route.ts
+++ src/app/api/testimonials/route.ts
@@ -1,29 +1,30 @@
-import UserModel from "@/app/models/User";
-import connectDb from "@/lib/connectDb";
+import UserModel from "@/app/models/User"
+import connectDb from "@/lib/connectDb"
 
-export async function GET(request: Request){
-    
-    const { searchParams } = new URL(request.url);
-    const queryParams = {
-        username: searchParams.get('username'),
-    };
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const queryParams = {
+    username: searchParams.get("username"),
+  }
 
-    const {username} = queryParams;
-    console.log("username: ", username)
+  const { username } = queryParams
+  console.log("username: ", username)
 
-    await connectDb();
-    try {
-        const response = await UserModel.findOne({username});
-        console.log("response: ", response)
-        return Response.json({
-            message: "Testimonials found",
-            data: response?.testimonials
-        })
-    } catch (error) {
-        return Response.json({
-            status: fail,
-            message: "Unexpeeected error", error
-        }, {status: 501})
-        
-    }
+  await connectDb()
+  try {
+    const response = await UserModel.findOne({ username })
+    console.log("response: ", response)
+    return Response.json({
+      message: "Testimonials found",
+      data: response?.testimonials,
+    })
+  } catch (error) {
+    return Response.json(
+      {
+        message: "Unexpected error",
+        error,
+      },
+      { status: 501 }
+    )
+  }
 }
