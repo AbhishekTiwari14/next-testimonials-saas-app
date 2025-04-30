a72dc6d19cd9780e0fc912c291a780e98efb40ed
AbhishekTiwari14 <abhishek.tiwari2003@gmail.com>
2025-04-30 16:58:23 +0530

    solvved cors issue

--- next.config.mjs
+++ next.config.mjs
@@ -5,17 +5,22 @@ const nextConfig = {
       {
         source: "/api/(.*)",
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:3001" }, // Replace with your actual allowed origin
-          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization"   
- },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Replace with your actual allowed origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
         ],
       },
-    ];
+    ]
   },
   images: {
-    domains: ['utfs.io'], 
+    domains: ["utfs.io"],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
